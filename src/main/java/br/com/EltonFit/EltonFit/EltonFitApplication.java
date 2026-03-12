264b422a50cd9676cdedace017583508644b6fda
Bernardo Braga <bebraga12@gmail.com>
2026-03-12 12:00:27 -0300

    Initial commit

---
+++ src/main/java/br/com/EltonFit/EltonFit/EltonFitApplication.java
@@ -0,0 +1,13 @@
+package br.com.EltonFit.EltonFit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EltonFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EltonFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/EltonFit/EltonFit/EltonFitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.EltonFit.EltonFit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EltonFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
